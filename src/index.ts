30b27b7126471b029821e5bbe8ed3416601b1c1f
Dan Fishgold <d.fishgold@gmail.com>
2018-06-09 18:23:00 +0300

    Add function to insert row into spreadsheet

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,53 @@
+import dotenv from "dotenv";
 import { authorize } from "./authorize";
 import { google } from "googleapis";
 
 authorize()
-  .then(auth => doStuff(auth))
+  .then(auth => addRow(auth, 2, ["אהלן", "גבר"]))
   .catch(err => console.log(`error: ${err}`));
 
-async function doStuff(auth: any) {
-  const drive = google.drive({ version: "v3", auth });
+async function addRow(auth: any, rowIndex: number, row: string[]) {
+  const spreadsheetId = process.env.spreadsheet_id;
+  const sheetName = process.env.sheet_name;
+
   const sheets = google.sheets({ version: "v4", auth });
 
-  const sheetData = await sheets.spreadsheets.values.get({
-    spreadsheetId: "1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms",
-    range: "Class Data!A2:E"
-  });
-  const rows = sheetData.data.values;
-  if (rows && rows.length) {
-    console.log("Name, Major:");
-    // Print columns A and E, which correspond to indices 0 and 4.
-    rows.map((row: any) => {
-      console.log(`${row[0]}, ${row[4]}`);
-    });
-  } else {
-    console.log("No data found.");
+  // Find the sheet id
+  const spreadsheet = await sheets.spreadsheets.get({ spreadsheetId });
+  if (!spreadsheet.data.sheets) {
+    throw "Couldn't load spreadsheet";
   }
+  const sheet = spreadsheet.data.sheets.find(
+    sheet => (sheet.properties ? sheet.properties.title == sheetName : false)
+  );
+  if (!sheet || !sheet.properties || sheet.properties.sheetId) {
+    throw `Couldn't find the sheet named "${sheetName}"`;
+  }
+  const sheetId = sheet.properties.sheetId;
+
+  // Insert a new empty row
+  const range = {
+    sheetId,
+    dimension: "ROWS",
+    startIndex: rowIndex - 1,
+    endIndex: rowIndex
+  };
+  await sheets.spreadsheets.batchUpdate({
+    spreadsheetId,
+    requestBody: { requests: [{ insertDimension: { range } }] }
+  });
+
+  // Fill in the new row
+  await sheets.spreadsheets.values.update({
+    spreadsheetId,
+    range: `${sheetName}!A${rowIndex}`,
+    valueInputOption: "USER_ENTERED",
+    requestBody: { values: [row] }
+  });
+}
+
+async function listFiles(auth: any) {
+  const drive = google.drive({ version: "v3", auth });
 
   const fileData = await drive.files.list({ pageSize: 10 });
   const files = fileData.data.files;
