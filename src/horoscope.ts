c42d7a55c544ce048b51eff3356d45051fb6eb75
Dan Fishgold <d.fishgold@gmail.com>
2018-06-10 20:02:29 +0300

    Remove useless fields

--- src/horoscope.ts
+++ src/horoscope.ts
@@ -3,32 +3,23 @@ import Telebot from "telebot";
 import Drive from "./drive";
 
 export enum Field {
-  Image,
-  Source,
-  Date,
   Sign,
   Content,
   Censor
 }
 
 export default class Horoscope {
-  image: string;
-  source?: string;
-  date?: string;
+  imageId: string;
   sign?: Signs.Sign;
   content?: string;
   censor?: string;
-  is_uploading: boolean;
 
-  constructor(image: string) {
-    this.image = image;
-    this.is_uploading = false;
+  constructor(imageId: string) {
+    this.imageId = imageId;
   }
 
-  public nextField(): Field | undefined {
-    if (!this.source) return Field.Source;
-    else if (!this.date) return Field.Date;
-    else if (!this.sign) return Field.Sign;
+  public nextEmptyField(): Field | undefined {
+    if (!this.sign) return Field.Sign;
     else if (!this.content) return Field.Content;
     else if (!this.censor) return Field.Censor;
     else return undefined;
@@ -36,10 +27,6 @@ export default class Horoscope {
 
   public parseFieldText(field: Field, text: string) {
     switch (field) {
-      case Field.Source:
-        this.source = text;
-      case Field.Date:
-        this.date = text;
       case Field.Sign:
         const parsed = Signs.fromString(text);
         if (parsed) this.sign = parsed;
@@ -53,13 +40,6 @@ export default class Horoscope {
 
   public requestField(field: Field, bot: Telebot, msg: any) {
     switch (field) {
-      case Field.Source:
-        return msg.reply("מאיזה עיתון זה?");
-      case Field.Date:
-        return msg.reply(
-          "ממתי זה? אני צריך תאריך בפורמט של שנה-חודש-יום. למשל 2018-05-16 עבור ה 16 במאי. אם זה מהיום תגיב/י ״היום״."
-        );
-
       case Field.Sign:
         return msg.reply("איזה מזל? (התחיל לרדת גשם)", {
           replyMarkup: Signs.keyboard(bot)
